3d73eab4b04f9b471715052a65768c810bf33db9
Mauricio N. Ferreira <contato@ferreiramauricio.com>
2017-10-27 09:59:19 -0200

    Adicionado estrutura básica do laravel

---
+++ PHP/projeto-laravel/resources/views/products/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <h1>Estou no arquivo index.blade.php da pasta products</h1>
+            </div>
+        </div>
+    </div>
+@endsection
